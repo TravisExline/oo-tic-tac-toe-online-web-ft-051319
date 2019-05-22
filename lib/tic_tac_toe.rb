43c5531e936b96aedc53dd007a4910e22a724985
Travis Exline <t.exline22@gmail.com>
2019-05-22 16:21:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,16 +47,15 @@ class TicTacToe
   end
 
   def turn
-    puts "Enter a move: 1-9"
+   puts "Please enter 1-9:"
     input = gets.strip
-    space = input_to_index(input)
-    if valid_move?(space)
-      move(space, current_player)
-      display_board
-    else
-      self.turn
+    index = input_to_index(input)
+     if valid_move?(index)
+       move(index, current_player)
+       display_board
+     else turn
     end
-  end
+   end 
 
   def won?
     WIN_COMBINATIONS.each do |win_combination|
