b72ebe90a857e1f7d3593cca147c9d715bee2070
Travis Exline <t.exline22@gmail.com>
2019-05-22 16:28:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,8 @@ class TicTacToe
     @board[position] != " "
   end
 
-  def valid_move?(position)
-    @board[position] == " "
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
