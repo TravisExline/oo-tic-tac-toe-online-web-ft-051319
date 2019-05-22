4c86de5a161244b9c14da10eb2879faa36899cd6
Travis Exline <t.exline22@gmail.com>
2019-05-22 13:48:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,8 +5,9 @@ class TicTacToe
     [6,7,8], #bot row
     [0,4,8], #L - R Diag top bot
     [2,4,6], #R-L Diag top bot
-    [0,3,6],
-    [2,5,8]
+    [0,3,6], #L down
+    [2,5,8], #R down
+    [1,4,7]
   ]
 
   def initialize
