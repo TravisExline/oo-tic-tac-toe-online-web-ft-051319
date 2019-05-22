7dfd81f0a96ec14cb7147945d0a0b889e3de3792
Travis Exline <t.exline22@gmail.com>
2019-05-22 13:47:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,14 @@
 class TicTacToe
-  WIN_COMBINATIONS = []
+  WIN_COMBINATIONS = [
+    [0,1,2], #top row
+    [3,4,5], #mid row
+    [6,7,8], #bot row
+    [0,4,8], #L - R Diag top bot
+    [2,4,6], #R-L Diag top bot
+    [0,3,6],
+    [2,8,8]
+  ]
+
   def initialize
     @board = Array.new(9, " ")
   end
